d6fb78ae93d617b15d561420fc49bebbf8a1b31a
Tobias Juarez <tojuarez@zurich-santander.com.ar>
2024-10-03 12:29:55 -0300

    Style changes and new component SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import Navbar from './Componentes/Navbar'
-import Links from './Componentes/Links'
-import Carrusel from './Componentes/Carrusel'
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './Componentes/Navbar';
+import Carrusel from './Componentes/Carrusel';
+import SignUp from './Componentes/SignUp';
+
 
 function App() {
 
@@ -9,12 +12,14 @@ function App() {
   ]
 
   return (
-    <div>
-      <Navbar/>
-      <Links/>
-      <Carrusel/>
-    </div>
-  ) //tengo que retornar dentro de app el componente en forma de etiqueta html siempre
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Carrusel slides={slides} />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  ); //tengo que retornar dentro de app el componente en forma de etiqueta html siempre
 }
 
 export default App
