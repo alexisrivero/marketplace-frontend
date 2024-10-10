661ecec6c9b363ddeeeb9c1f86e8e6a023807fd2
Tobias Juarez <tojuarez@zurich-santander.com.ar>
2024-10-10 16:35:43 -0300

    addition of categories to the navbar

--- src/Views/NavBar/Links.jsx
+++ src/Views/NavBar/Links.jsx
@@ -1,14 +1,27 @@
 import "./Links.css"
+import React, {useState} from "react";
 import { MdMenu } from "react-icons/md";
 
 const Links = () => {
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+    const showDropdown = () => {
+        setDropdownVisible(true);
+    };
+
+    const hideDropdown = () => {
+        setDropdownVisible(false);
+    };
+
     return (
         <section className="links"> 
             <nav>
                 <ul>
-                    <li>
-                        <a href="/categories"><MdMenu size={23}/></a>
-                        <a href="/categories">Categorias</a>
+                    <li onMouseOver={showDropdown} onMouseLeave={hideDropdown}>
+                        <a>
+                            <MdMenu size={23}/> 
+                        </a>
+                        <a>Categorias</a>
                     </li>
                     <li>
                         <a href="/contact">Contacto</a>
@@ -21,6 +34,13 @@ const Links = () => {
                     </li>
                 </ul>
             </nav>
+            {isDropdownVisible && (
+                            <ul className="dropdown" onMouseOver={showDropdown} onMouseLeave={hideDropdown}>
+                                <li><a href="/categories/1">Categoria 1</a></li>
+                                <li><a href="/categories/2">Categoria 2</a></li>
+                                <li><a href="/categories/3">Categoria 3</a></li>
+                            </ul>
+                        )}
         </section>
     )
 }
