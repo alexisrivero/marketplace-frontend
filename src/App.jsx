0603f10b3ccfef6e2136674a49fd22de034338f0
Tobias Juarez <tojuarez@zurich-santander.com.ar>
2024-10-10 14:59:01 -0300

    views added and components moved to views, also added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navbar from './Componentes/NavBar/Navbar';
-import Home from './Componentes/Home/Home';
-import SignUp from './Componentes/SignUp/SignUp';
-import SignIn from './Componentes/SignIn/SignIn';
+import Navbar from './Views/NavBar/Navbar';
+import Home from './Views/Home/Home';
+import SignUp from './Views/SignUp/SignUp';
+import SignIn from './Views/SignIn/SignIn';
+import Footer from './Views/Footer/Footer';
 
 function App() {
 
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
-    </Router>
+      <Footer />
+      </>
   ); 
 }
 
---
+++ src/Views/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+import "./Footer.css";
+
+
+const Footer = () => {
+    return (
+        <footer>
+        <div className="exclusive-container">
+            <div className="tittle">
+                <h3>Exclusivo</h3>
+            </div>
+            <div className="content">
+            <Link to="/sign-up">Registrate</Link>
+            </div>
+            <p>Obtene un 10% de descuento en tu primera compra</p>
+        </div>
+        <div className="contact-container">
+            <div className="tittle">
+                <h3>Contacto</h3>
+            </div>
+            <div className="content">
+                <p>Av. Siempreviva 742, Buenos Aires, Argentina</p>
+                <p>+54 123456789</p>
+                <p>marketplace@gmail.com.ar </p>
+            </div>
+        </div> 
+        <div className="account-container">
+            <div className="tittle">
+                <h3>Cuenta</h3>
+            </div>
+            <div className="content">
+                <Link to="/sign-in">Iniciar sesión</Link>
+                <Link to="/sign-up">Registrarse</Link>
+            </div>
+        </div>
+        <div className="social-container">
+            <div className="tittle">
+                <h3>Seguinos en nuestras redes</h3>
+            </div>
+            <div className="content">
+                <Link to="facebook.com"><FaFacebookSquare size={25}/></Link>
+                <Link to="instagram.com"><FaInstagramSquare size={25}/></Link>
+                <Link to="twitter.com"><FaTwitterSquare size={25}/></Link>
+            </div>
+        </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
