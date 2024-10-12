db7a8b10bac7227baee9cc608a11d04ed590002e
Tobias Juarez <tojuarez@uade.edu.ar>
2024-10-12 01:06:15 -0300

    API implementation in products and categories

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Carrusel from '../../Componentes/Carrusel/Carrusel';
 import ProductList from "../../Componentes/ProductList/ProductList";
 import './Home.css';
+import React, {useEffect, useState} from 'react';
 
 
 const Home = () => {
@@ -11,37 +12,31 @@ const Home = () => {
         {url: "http://localhost:5173/image4.jpg", title: "Publicidad4"},
         {url: "http://localhost:5173/image5.jpg", title: "Publicidad5"}
     ]
+
+    const [data, setData] = useState([]);
+
+    const URL = 'http://localhost:8080/product';
     
     const conteinerStyle = {
         width: '500px',
         height: '280px',
         margin: '0px auto'
     }
-    const products = [
-        {
-            image: "https://images.fravega.com/f300/d504221275bde1ac2409394cf4455e3c.jpg.webp",
-            name: "Producto 1",
-            description: "Descripcion del producto 1",
-            price: 100
-        },
-        {
-            image: "https://images.fravega.com/f300/d504221275bde1ac2409394cf4455e3c.jpg.webp",
-            name: "Producto 2",
-            description: "Descripcion del producto 2",
-            price: 200
-        },
-        {
-            image: "https://images.fravega.com/f300/d504221275bde1ac2409394cf4455e3c.jpg.webp",
-            name: "Producto 3",
-            description: "Descripcion del producto 3",
-            price: 300
-        }
-    ]
+    
+
+    useEffect(() => {
+        fetch(URL)
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+
 
     return (
         <div>
             <Carrusel slides={slides} />
-            <ProductList products={products} />
+            <ProductList products={data} />
         </div>
     )
 }
--- src/Views/NavBar/Links.jsx
+++ src/Views/NavBar/Links.jsx
@@ -1,9 +1,21 @@
 import "./Links.css"
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { MdMenu } from "react-icons/md";
 
 const Links = () => {
     const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+    const URL = 'http://localhost:8080/product/category';
+
+    const [categories, setCategories] = useState([]);
+    
+    
+    useEffect(() => {
+        fetch(URL)
+        .then(response => response.json())
+        .then(data => setCategories(data))
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
     const showDropdown = () => {
         setDropdownVisible(true);
@@ -36,9 +48,11 @@ const Links = () => {
             </nav>
             {isDropdownVisible && (
                             <ul className="dropdown" onMouseOver={showDropdown} onMouseLeave={hideDropdown}>
-                                <li><a href="/categories/1">Categoria 1</a></li>
-                                <li><a href="/categories/2">Categoria 2</a></li>
-                                <li><a href="/categories/3">Categoria 3</a></li>
+                                {categories.map(category => (
+                                    <li key={category.id}>
+                                        <a href={`/category/${category.id}`}>{category}</a>
+                                    </li>
+                                ))}
                             </ul>
                         )}
         </section>
