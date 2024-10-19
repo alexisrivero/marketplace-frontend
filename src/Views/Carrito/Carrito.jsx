30a0334bde6a62d96146142613046e6a3b4656dd
Tobias Juarez <tojuarez@uade.edu.ar>
2024-10-19 00:55:29 -0300

    implementacion de carrito

--- src/Views/Carrito/Carrito.jsx
+++ src/Views/Carrito/Carrito.jsx
@@ -2,28 +2,66 @@ import './Carrito.css';
 import React, { useEffect, useState } from 'react';
 
 const Carrito = () => {
-    const URL = 'http://localhost:8080/cart';
+    const URL = 'http://localhost:8080/checkout';
     const [data, setData] = useState([]);
     useEffect(() => {
-        fetch(URL)
+        fetch(URL, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+            },
+        })
         .then(response => response.json())
         .then(data => setData(data))
         .catch(error => console.error('Error fetching data:', error));
     }, []);
+    console.log(data);
+    
     return (
-        <div>
-            <h1>Carrito</h1>
-            <div className="cart-container">
-                {data.map(product => (
-                    <div key={product.id} className="cart-product">
-                        <img src={product.imageRoute} alt={product.name} className="cart-product-image" />
-                        <div className="cart-product-data">
-                            <h2 className="cart-product-name">{product.name}</h2>
-                            <p className="cart-product-price">${product.price}</p>
-                            <p className="cart-product-quantity">Cantidad: {product.quantity}</p>
-                        </div>
+        <div className='carrito'>
+            <div className='lista-carrito'>
+                <h1>Carrito</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.checkoutProducts?.map(product => (
+                            <tr key={product.id}>
+                                <td className='product'>
+                                    <img src={product.imageRoute} alt={product.name} style={{ width: '50px', height:'50px' }} />
+                                {product.name}</td>
+                                <td>${product.price}</td>
+                                <td>{product.quantity}</td>
+                                <td>${(product.price*product.quantity).toFixed(2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className='container-precio-total'>
+                <div className='precio-total'>
+                    <h3>Precio total</h3>
+                    <div className='subtotal'>
+                        <p>Subtotal:</p>
+                        <p>${data.subTotal}</p>
                     </div>
-                ))}
+                    <div className='subtotal'>
+                        <p>Envio:</p>
+                        <p>Gratis</p>
+                    </div>
+                    <div className='total'>
+                        <p>Total:</p>
+                        <p>${data.subTotal}</p>
+                    </div>
+                    <div className='button'>
+                        <button className='comprar'>Comprar</button>  
+                    </div>  
+                </div>
             </div>
         </div>
     );
